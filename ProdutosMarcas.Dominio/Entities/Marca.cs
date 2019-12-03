bbcc2de3d951f62004e2f6156b0bc5a6a5a55f5c
PauloAlves8039 <pj.alves547@gmail.com>
2019-12-03 16:26:12 -0300

    Commit inicial do repositório

---
+++ ProdutosMarcas.Dominio/Entities/Marca.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Arquivo: classe Marca
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter propriedades da entidade Marca
+ * Data: 03/12/2019
+*/
+
+using System.Collections.Generic;
+
+namespace ProdutosMarcas.Dominio.Entities
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public virtual List<Produto> Produtos { get; set; }
+    }
+}
---
+++ ProdutosMarcas.Dominio/Entities/Produto.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Arquivo: classe Produto
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter propriedades da entidade Produto
+ * Data: 03/12/2019
+*/
+
+namespace ProdutosMarcas.Dominio.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int MarcarId { get; set; }
+        public virtual Marca Marca { get; set; }
+    }
+}
